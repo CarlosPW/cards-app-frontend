fb3296780a42bfc961d060e3efec6c35499c856a
Carlos Peñaloza <carlos818k@gmail.com>
2022-07-04 17:43:59 -0400

    upd

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-import Cookies from "js-cookie";
 import { jwt } from "../utils";
 
 // This function can be marked `async` if using `await` inside
