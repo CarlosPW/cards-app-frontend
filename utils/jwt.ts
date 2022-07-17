5aef9d4983a00a7b363d05768839263d6ad9fdbf
Carlos Peñaloza <carlos818k@gmail.com>
2022-07-17 17:38:58 -0400

    updt jwtVerify

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -43,13 +43,13 @@ export async function sign(payload: string, secret: string): Promise<string> {
 }
 
 export async function isValidToken(token: string): Promise<JWTPayload> {
-  const { payload } = await jwtVerify(
-    token,
-    new TextEncoder().encode(process.env.JWT_SECRET)
-  );
-  // run some checks on the returned payload, perhaps you expect some specific values
-
-  // if its all good, return it, or perhaps just return a boolean
-
-  return payload;
+  try {
+    const { payload } = await jwtVerify(
+      token,
+      new TextEncoder().encode(process.env.JWT_SECRET)
+    );
+    return payload;
+  } catch (error) {
+    throw new Error("Failed Token");
+  }
 }
